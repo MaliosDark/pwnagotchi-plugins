ce2e11572e0f95cf16878fe8d67503646fe59526
MaliosDark <139622527+MaliosDark@users.noreply.github.com>
2024-01-14 19:58:02 +0000

    Update educational-purposes-exclusively.py

--- educational-purposes-exclusively.py
+++ educational-purposes-exclusively.py
@@ -16,13 +16,13 @@ CHANNEL = 0
 
 class EducationalPurposesOnly(Plugin):
     __author__ = '@nagy_craig , MaliosDark'
-    __version__ = '1.0.10'
+    __version__ = '1.0.11'
     __license__ = 'GPL3'
     __description__ = 'A plugin to automatically authenticate to known networks and perform internal network recon'
 
     def on_loaded(self):
         global READY
-        logging.info("educational-purposes-only loaded")
+        logging.info("educational-purposes-exclusively loaded")
         READY = 1
     
     def display_text(self, text):
@@ -30,30 +30,40 @@ class EducationalPurposesOnly(Plugin):
         STATUS = text
     
     def on_ui_update(self, ui):
-        global STATUS
-        if STATUS == 'rssi_low':
-            ui.set('face', '(ﺏ__ﺏ)')
-            ui.set('status', 'Signal strength of %s is currently too low to connect ...' % NETWORK)
-        elif STATUS == 'home_detected':
-            ui.set('face', '(◕‿‿◕)')
-            ui.set('face', '(ᵔ◡◡ᵔ)')
-            ui.set('status', 'Found home network at %s ...' % NETWORK)
-        elif STATUS == 'switching_mon_off':
-            ui.set('face', '(◕‿‿◕)')
-            ui.set('face', '(ᵔ◡◡ᵔ)')
-            ui.set('status', 'We\'re home! Pausing monitor mode ...')
-        elif STATUS == 'scrambling_mac':
-            ui.set('face', '(⌐■_■)')
-            ui.set('status', 'Scrambling MAC address before connecting to %s ...' % NETWORK)
-        elif STATUS == 'associating':
-            ui.set('status', 'Greeting the AP and asking for an IP via DHCP ...')
-            ui.set('face', '(◕‿◕ )')
-            ui.set('face', '( ◕‿◕)')
-        elif STATUS == 'associated':
-            ui.set('face', '(ᵔ◡◡ᵔ)')
-            ui.set('status', 'Home at last!')
-            ui.set('status', f'Connected to {NETWORK} on channel {CHANNEL}.')
-            ui.set('status', 'Performing network reconnaissance...')
+    global STATUS
+    status_messages = []
+
+    if STATUS == 'rssi_low':
+        ui.set('face', '(⊙_☉)')
+        status_messages.append('Signal strength of %s is currently too low to connect ...' % NETWORK)
+        status_messages.append(f'Interface Status: {"Up" if "UP" in os.popen("ifconfig wlan0mon").read() else "Down"}')
+    elif STATUS == 'home_detected':
+        ui.set('face', '(╯°□°）╯︵ ┻━┻')
+        ui.set('face', '(⚆_⚆)')
+        status_messages.append('Found home network at %s ...' % NETWORK)
+    elif STATUS == 'switching_mon_off':
+        ui.set('face', '(◔_◔)')
+        ui.set('face', '(¬_¬)')
+        status_messages.append('We\'re home! Pausing monitor mode ...')
+    elif STATUS == 'scrambling_mac':
+        ui.set('face', '(¬‿¬)')
+        status_messages.append('Scrambling MAC address before connecting to %s ...' % NETWORK)
+    elif STATUS == 'associating':
+        status_messages.append('Greeting the AP and asking for an IP via DHCP ...')
+        ui.set('face', '(¬◡¬ )')
+        ui.set('face', '( ¬◡¬)')
+    elif STATUS == 'associated':
+        ui.set('face', '(¬‿¬)')
+        status_messages.append(f'Signal Strength: {signal_strength} dBm')
+        status_messages.append('Home at last!')
+        status_messages.append(f'Connected to {NETWORK} on channel {CHANNEL}.')
+        current_mac = os.popen("ifconfig wlan0mon | grep -o -E '([0-9a-fA-F]:?){2,6}'").read().strip()
+        status_messages.append(f'Current MAC Address: {current_mac}')
+        status_messages.append('Performing network reconnaissance...')
+
+    # Única llamada para actualizar el estado
+    ui.set('status', '\n'.join(status_messages))
+
 
      
     def _connect_to_target_network(self, network_name, channel, interface='wlan0mon'):
@@ -146,22 +156,21 @@ class EducationalPurposesOnly(Plugin):
             self._restart_monitor_mode()
         
     def on_wifi_update(self, agent, access_points):
-        global READY
         global STATUS
         home_network = self.options['home-network']
-        if READY == 1 and "Not-Associated" in os.popen('iwconfig wlan0monmon').read():
-            for network in access_points:
-                if network['hostname'] == home_network:
-                    signal_strength = network['rssi']
-                    channel = network['channel']
-                    logging.info("FOUND home network nearby on channel %d (rssi: %d)" % (channel, signal_strength))
-                    if signal_strength >= self.options['minimum-signal-strength']:
-                        logging.info("Starting association...")
-                        READY = 0
-                        self._connect_to_target_network(network['hostname'], channel)
-                    else:
-                        logging.info("The signal strength is too low (%d) to connect." % (signal_strength))
-                        STATUS = 'rssi_low'
+        
+        for network in access_points:
+            if network['hostname'] == home_network:
+                signal_strength = network['rssi']
+                channel = network['channel']
+                logging.info("FOUND home network nearby on channel %d (rssi: %d)" % (channel, signal_strength))
+                if signal_strength >= self.options['minimum-signal-strength']:
+                    logging.info("Starting association...")
+                    self._connect_to_target_network(network['hostname'], channel)
+                else:
+                    logging.info("The signal strength is too low (%d) to connect." % (signal_strength))
+                    STATUS = 'rssi_low'
+
 
     def _port_scan(self, target_ip):
         open_ports = []

e2f01207fe00df8a1d07760d926efe7c05f5fcdd
Andryu Schittone <139622527+MaliosDark@users.noreply.github.com>
2024-04-04 22:28:33 +0100

    Update test_security.py

--- test_security.py
+++ test_security.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 class SecurityMonitor(plugins.Plugin):
     __GitHub__ = "https://github.com/MaliosDark/pwnagotchi-plugins"
     __author__ = "MaliosDark, (itsdarklikehell bauke.molenaar@gmail.com)"
-    __version__ = "1.0.6.2b"
+    __version__ = "1.0.7"
     __license__ = "GPL3"
     __description__ = "LAN Security Monitor Plugin for Pwnagotchi"
     __name__ = "SecurityMonitor"
